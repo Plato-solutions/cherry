c15a90848857912a96606fbe583dd681293192bc
Ryan Miller <7181449-platosolutions@users.noreply.gitlab.com>
2022-02-06 19:20:38 -0500

    added update to schema

--- cherry/src/schema.rs
+++ cherry/src/schema.rs
@@ -2,10 +2,11 @@ use crate::types::{Arguments, Result, Row};
 
 use std::any::TypeId;
 use crate::{connection};
-use crate::query::select::Select;
 use crate::types::{Pool};
 use crate::types::Transaction;
 use async_trait::async_trait;
+use crate::query::select::Select;
+use crate::query::update::Update;
 
 #[async_trait]
 pub trait Schema: Sized + Send + Unpin {
@@ -31,8 +32,40 @@ pub trait Schema: Sized + Send + Unpin {
     // }
 
     /// Insert a row into the database.
-    fn select<'a>(
-    ) -> Select<'a,Self> {
+    fn select<'a>() -> Select<'a,Self> {
         Select::new(Self::datasource())
     }
+
+    fn update<'a>() -> Update<'a> {
+        Update::new::<Self>(Self::datasource())
+    }
 }
+
+// fn insert<'a, T>(&'static self, v: &'a T) -> Insert<'a> where T: Schema + 'static {
+//     Insert::insert(self.type_id(),  v)
+// }
+//
+// fn insert_bulk<'a, T>(&'static self, v: &'a [T]) -> Insert<'a> where T: Schema + 'static {
+//     Insert::insert_bulk(self.type_id(), v)
+// }
+//
+// fn insert_ignore<'a, T>(&'static self, v: &'a [T]) -> Insert<'a> where T: Schema + 'static {
+//     Insert::insert_ignore(self.type_id(), v)
+// }
+//
+// fn insert_replace<'a, T>(&'static self, v: &'a [T]) -> Insert<'a> where T: Schema + 'static {
+//     Insert::insert_replace(self.type_id(), v)
+// }
+//
+// fn insert_update<'a, T>(&'static self, v: &'a [T]) -> InsertUpdate<'a>
+//     where T: Schema + 'static {
+//     InsertUpdate::insert_update(self.type_id(), v)
+// }
+//
+// fn delete<'a, T>(&'static self) -> Delete<'a> where T: Schema + 'static {
+//     Delete::new::<T>(self.type_id())
+// }
+//
+// async fn begin<'a>(&'static self) -> Result<Transaction<'a>>  {
+//     Ok(connection::get(self.type_id())?.begin().await?)
+// }
